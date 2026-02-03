54f44c80b71e219d201fc2daa79490e070b36eb8
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-02-03 19:48:30 -0300

    fix: add Vercel production URL to CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173", 
+    "https://my-portifolio-sandy-one.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
